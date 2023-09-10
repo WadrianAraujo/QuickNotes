91bf035c757e1b349b10145b50be209f6c7046ea
Wadrian Araujo <wadm.snf20@uea.edu.br>
2023-09-09 20:14:56 -0400

    Migrate from kapt to KSP annotations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
-    id("kotlin-kapt")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -43,9 +43,9 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.core:core-ktx:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.5.1")
+    implementation("com.google.android.material:material:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -55,9 +55,9 @@ dependencies {
 
     implementation("androidx.room:room-ktx:$roomVersion")
     implementation("androidx.room:room-runtime:$roomVersion")
-    kapt("androidx.room:room-compiler:$roomVersion")
+    ksp("androidx.room:room-compiler:$roomVersion")
 
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.9")
 }
--- app/src/main/java/com/weadrix/quicknotes/Database/NoteDao.kt
+++ app/src/main/java/com/weadrix/quicknotes/Database/NoteDao.kt
@@ -20,6 +20,7 @@ interface NoteDao{
     @Query("Select * from notes_table order by id ASC")
     fun getAllNotes() : LiveData<List<Note>>
 
-    @Query("UPDATE notes_table Set title = :title, note = :note WHERE id = :id")
-    suspend fun update(id : Int?, title : String?, note : String?)
+    @Query("UPDATE notes_table SET title = :title, note = :note WHERE id = :id")
+    suspend fun update(id: Int?, title: String?, note: String?)
+
 }
